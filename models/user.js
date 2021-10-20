d9942a88d13da1d4fa7aa16db2d4390c3ab34d00
Angel Mejia <anglmejia09@gmail.com>
2021-10-20 14:32:35 -0500

    Primer commit backend v1

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const db = require('../config/config');
+
+const User = {};
+
+User.getAll = () => {
+    const sql = `
+    SELECT
+        *
+    FROM 
+        users
+    `;
+    return db.manyOrNone(sql);
+}
+
+User.create = (user) => {
+    const sql =
+    `INSERT INTO
+        users(
+            email,
+            name,
+            lastname,
+            phone,
+            password,
+            created_at,
+            updated_at
+            )
+        VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id
+        `;
+        
+        return db.oneOrNone(sql, [
+            user.email,
+            user.name,
+            user.lastname,
+            user.phone,
+            user.password,
+            new Date(),
+            new Date()
+        ]);
+}
+
+module.exports = User;
---
+++ routes/usersRoutes.js
@@ -0,0 +1,9 @@
+const UsersController = require('../controllers/usersController');
+
+module.exports = (app) => {
+    
+    app.get('/api/users/getAll',UsersController.getAll);
+
+    app.post('/api/users/create',UsersController.register);
+
+}
